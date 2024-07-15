9f9bfad7c2dde99bd082773941f1141b68c7fdbe
Neata Alexandru <frez758@gmail.com>
2024-07-15 20:01:32 +0300

    Added the ability to change the multiplier for each building individually and bumped the version

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // How fast the construction should be. Default is 10% (10).
-    hideoutConstructionMultiplier: 10
-};
--- src/mod.ts
+++ src/mod.ts
@@ -5,7 +5,7 @@ import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { DatabaseService } from "@spt/services/DatabaseService";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
-import configuration from "../config";
+import configuration from "../config.json";
 
 class Mod implements IPostDBLoadMod
 {
@@ -21,6 +21,9 @@ class Mod implements IPostDBLoadMod
         // Get all the hideout areas
         const areas = tables.hideout.areas;
 
+        // Array for changes to log
+        const changes = [];
+
         areas.forEach((area) =>
         {
             Object.keys(area.stages).forEach((stageKey) =>
@@ -31,19 +34,35 @@ class Mod implements IPostDBLoadMod
                 // Change the construction time of the stage if it's not 0
                 if (stage.constructionTime > 0)
                 {
-                    // Calculate the new construction time
-                    const newTime = Math.round(stage.constructionTime * configuration.hideoutConstructionMultiplier / 100);
+                    // Extract the current construction time
+                    let constructionTime = stage.constructionTime;
 
-                    stage.constructionTime = newTime;
+                    // Check if the area has a multiplier
+                    const configArea = configuration.buildings.find((configArea) => configArea.id === area._id);
+
+                    if (configArea && configArea.multiplier !== 1)
+                    {
+                        // Calculate the new construction time
+                        constructionTime = Math.round(stage.constructionTime * configArea.multiplier / 100);
+                        changes.push(`Changed the construction time of the ${configArea.name} ${parseInt(stageKey) + 1} to: ${configArea.multiplier}% of the original time.`);
+                    }
+                    else
+                    {
+                        // Calculate the new construction time
+                        constructionTime = Math.round(stage.constructionTime * configuration.globalHideoutConstructionMultiplier / 100);
+                    }
+
+                    stage.constructionTime = constructionTime;
                 }
             })
         })
 
         // Log the changes
         logger.logWithColor(
-            `[VariableConstructionTime] Changed the construction time of the hideout stages to: ${configuration.hideoutConstructionMultiplier}% of the original time.`,
+            `[VariableConstructionTime] Changed the construction time of the hideout stages to: ${configuration.globalHideoutConstructionMultiplier}% of the original time.`,
             LogTextColor.GREEN
         );
+
     }
 }
 
